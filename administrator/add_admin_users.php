50a1b76025e80aeb54181e3fad1f78e85a4c793b
Srinivaslancius <srinivas@lanciussolutions.com>
2017-07-10 18:23:00 +0530

    my proj commit

--- administrator/add_admin_users.php
+++ administrator/add_admin_users.php
@@ -2,7 +2,7 @@
 <div class="mn-content fixed-sidebar">
  <header class="mn-header navbar-fixed">
     <?php include_once'header.php';?>
-</header>
+</header> 
 
 <?php include_once 'side_navigation.php';?>
 <?php  if (!isset($_POST['submit']))  {

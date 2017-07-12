793134e0632d9764cd809c18e2ddeb823bd899cb
gunavardhanlancius <gunavardhan@lanciussolutions.com>
2017-07-12 19:02:28 +0200

    worked on categories

--- admin/add_categories.php
+++ admin/add_categories.php
@@ -1,9 +1,5 @@
-       
-<div class="mn-content fixed-sidebar">
- <header class="mn-header navbar-fixed">
-    <?php include_once'header.php';?>
-</header>
-
+<?php include_once 'main_header.php'; ?>
+           
 <?php include_once 'side_navigation.php';?>
 <?php  if (!isset($_POST['submit']))  {
             echo "";
--- admin/categories.php
+++ admin/categories.php
@@ -1,46 +1,37 @@
-   
-<div class="mn-content fixed-sidebar">
-<header class="mn-header navbar-fixed">
-<?php include_once'header.php';?>
-</header>
-<link href="assets/plugins/datatables/css/jquery.dataTables.min.css" rel="stylesheet">
-<main class="mn-inner">
-<div class="row">
-<div class="col s12">
-    <div class="page-title">Categories</div>
-</div>
-<?php $getData = getAllData('categories'); ?>
-<div class="col s12 m12 l12">
-    <div class="card">
-        <div class="card-content">                               
-           <a href="add_categories.php" style="float:right">Add Categories</a>
-            <table id="example" class="display responsive-table datatable-example">
-                <thead>
-                    <tr>
-                        <th>Category Name</th>
-                        <th>Category Image </th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                
-                <tbody>
-                    <?php while ($row = $getData->fetch_assoc()) { ?>
-                    <tr>
-                        <td><?php echo $row['category_name'];?></td>
-                        <td><img src="<?php echo $base_url . 'uploads/category_images/'.$row['category_image'] ?>" height="50px" width="50px"/></td>                        
-                        <td><a href="edit_categories.php?uid=<?php echo $row['id']; ?>">Edit</a>&nbsp;&nbsp;<a href="">Delete</a></td>
-                    </tr>               
-                    <?php } ?>
-                </tbody>
+            <?php include_once 'main_header.php'; ?>
+           
+            <?php include_once 'side_navigation.php';?>
 
-            </table>
-        </div>
-    </div>
-</div>
-</div>
-</main>
-
-</div>
-
-
-<?php include_once 'footer.php'; ?>
+            <main class="mn-inner">
+                <div class="row">
+                    
+                    <div class="col s12 m12 l12">
+                        <div class="card">
+                            <div class="card-content">
+                                 <a href="add_categories.php" style="float:right">Add Categories</a>
+                                <span class="card-title">Users</span>
+                                <?php $getData = getAllData('categories'); ?>
+                                <table id="example" class="display responsive-table datatable-example">
+                                    <thead>
+                                        <tr>
+                                            <th>Category Name</th>
+                                            <th>Category Image </th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>                                    
+                                    <tbody>
+                                        <?php while ($row = $getData->fetch_assoc()) { ?>
+                                        <tr>
+                                            <td><?php echo $row['category_name'];?></td>
+                                            <td><img src="<?php echo $base_url . 'uploads/category_images/'.$row['category_image'] ?>" height="50px" width="50px"/></td>                        
+                                            <td><a href="edit_categories.php?uid=<?php echo $row['id']; ?>">Edit</a>&nbsp;&nbsp;<a href="">Delete</a></td>
+                                        </tr>               
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+         <?php include_once 'footer.php'; ?>

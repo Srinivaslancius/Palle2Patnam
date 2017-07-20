f8dd3883b2bb77d99359b835f4e178a548134150
Srinivaslancius <srinivas@lanciussolutions.com>
2017-07-20 11:34:15 +0530

    Add srtyles for new popup

--- admin/products.php
+++ admin/products.php
@@ -29,15 +29,13 @@
                                             <div id="myModal_<?php echo $row['id']; ?>" class="modal fade" >
                                                 <div class="modal-dialog" Style="margin-top:10%;">
                                                     <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                                        <div class="modal-header">                                                            
                                                             <h3 class="modal-title"><b>Product Information</b> </h3>
                                                         </div>
                                                         <div class="modal-body" >
-                                                            <h5><b>Name</b></h5><?php echo $row['product_name']."<br>";?>
-                                                            <h5><b>Status</b></h5><?php if($row['status'] == 0){ echo "Active";}else{ echo "In Active";}?>
+                                                            <h5 class="modal-title-set"><b>Name : </b><?php echo $row['product_name'];?></h5>
+                                                            <h5 class="modal-title-set"><b>Status :</b><?php if($row['status'] == 0){ echo "Active";}else{ echo "In Active";}?></h5>
                                                         </div>
-
                                                         <div class="modal-footer" >
                                                               <button type="button" class="btn" data-dismiss="modal" style="background-color:#f00; color:#fff">Close</button>
                                                         </div>
@@ -54,9 +52,8 @@
                     </div>
                 </div>
             </main>
-         <?php include_once 'footer.php'; ?>
-         <!-- model pop-up Script for all pages with bootstrap js -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+         <?php include_once 'footer.php'; ?>         
+         <!-- model pop-up Script for all pages with bootstrap js -->        
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script type="text/javascript">
             $(document).ready(function(){

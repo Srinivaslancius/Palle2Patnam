ee879e4e5a47bbb06beb1c8a5b6b47dab097e426
gunavardhanlancius <gunavardhan@lanciussolutions.com>
2017-08-08 19:10:56 +0530

    git issues checking with guna

--- admin/TCPDF/examples/monthly_vendor_pdf_reports.php
+++ admin/TCPDF/examples/monthly_vendor_pdf_reports.php
@@ -33,6 +33,7 @@ $price = 0;
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Include the main TCPDF library (search for installation path).
 require_once('tcpdf_include.php');
+include_once('../../includes/functions.php');
 
 // extend TCPF with custom functions
 class MYPDF extends TCPDF {
@@ -44,28 +45,19 @@ class MYPDF extends TCPDF {
         $from_change_format =  date("Y-m-d", strtotime($_GET['start_date']));
         $to_change_format =  date("Y-m-d", strtotime($_GET['end_date']));
         if($from_change_format!='1970-01-01' && $to_change_format!='1970-01-01') {
-        $getSelData = "SELECT users.user_name,users.street_name,users.user_mobile,milk_orders.start_date,milk_orders.end_date,milk_orders.total_ltr AS actual_ltrs ,milk_orders.price_ltr,SUM(extra_milk_orders.extra_ltr) AS extra_ltrs,SUM(cancel_milk_orders.cancel_ltr) AS cancel_ltrs,milk_orders.total_ltr + SUM(extra_milk_orders.extra_ltr)-SUM(cancel_milk_orders.cancel_ltr) AS grand_total_milk ,users.id FROM users LEFT JOIN extra_milk_orders
-          ON users.id = extra_milk_orders.user_id LEFT JOIN cancel_milk_orders
-          ON users.id = cancel_milk_orders.user_id LEFT JOIN milk_orders ON users.id = milk_orders.user_id WHERE DATE_FORMAT(milk_orders.start_date,'%Y-%m-%d') between '$from_change_format' AND '$to_change_format' group by users.id";
-
+            $getSelData = "SELECT id,milk_in_ltrs,SUM(milk_in_ltrs) AS total_milk_vendor_ltrs from `vendor_milk_assign` WHERE DATE_FORMAT(created_date,'%Y-%m-%d') between '$from_change_format' AND '$to_change_format' ORDER BY id DESC";
         } else {
-            $getSelData = "SELECT users.user_name,users.street_name,users.user_mobile,milk_orders.start_date,milk_orders.end_date,milk_orders.total_ltr AS actual_ltrs ,milk_orders.price_ltr,SUM(extra_milk_orders.extra_ltr) AS extra_ltrs,SUM(cancel_milk_orders.cancel_ltr) AS cancel_ltrs,milk_orders.total_ltr + SUM(extra_milk_orders.extra_ltr)-SUM(cancel_milk_orders.cancel_ltr) AS grand_total_milk ,users.id FROM users LEFT JOIN extra_milk_orders
-          ON users.id = extra_milk_orders.user_id LEFT JOIN cancel_milk_orders
-          ON users.id = cancel_milk_orders.user_id LEFT JOIN milk_orders ON users.id = milk_orders.user_id group by users.id";
-        }
+            echo $getSelData = "SELECT id,milk_in_ltrs,SUM(milk_in_ltrs) AS total_milk_vendor_ltrs from `vendor_milk_assign` ORDER BY id DESC ";
+        }        
+
         $resultset = mysqli_query($conn, $getSelData) or die("database error:". mysqli_error($conn));
         //$lines = file($resultset);
         //echo "<pre>"; print_r($resultset); die;
         $data = array();
         while ($milkOrderData= mysqli_fetch_array($resultset)){
-            //echo "<pre>"; print_r($milkOrderData);    
-            //$values = implode(';', $milkOrderData);
-            //array_push($data, $values);
+            //echo "<pre>"; print_r($milkOrderData);                
             $data[] = $milkOrderData;
-        }
-        /*foreach($lines as $line) {
-            $data[] = explode(';', chop($line));
-        }*/
+        }       
         return $data;
     }
 
@@ -78,7 +70,7 @@ class MYPDF extends TCPDF {
         $this->SetLineWidth(0.3);
         $this->SetFont('', 'B');
         // Header
-        $w = array(20, 20, 18, 18, 18, 18, 18, 18, 20, 13);
+        $w = array(30, 30, 30);
         $num_headers = count($header);
         for($i = 0; $i < $num_headers; ++$i) {
             $this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', 1);
@@ -90,17 +82,13 @@ class MYPDF extends TCPDF {
         $this->SetFont('');
         // Data
         $fill = 0;
+         
         foreach($data as $row) {
-            $this->Cell($w[0], 6, $row[0], 'LR', 0, 'L', $fill);
-            $this->Cell($w[1], 6, $row[1], 'LR', 0, 'L', $fill);
+            $getVendorName = getIndividualDetails($row[0],'vendors','id');
+            $this->Cell($w[0], 6, $getVendorName['vendor_name'], 'LR', 0, 'L', $fill);
+            $this->Cell($w[1], 6, "Milk", 'LR', 0, 'L', $fill);
             $this->Cell($w[2], 6, $row[2], 'LR', 0, 'L', $fill);
-            $this->Cell($w[3], 6, $row[3], 'LR', 0, 'L', $fill);
-            $this->Cell($w[4], 6, $row[4], 'LR', 0, 'L', $fill);
-            $this->Cell($w[5], 6, $row[5], 'LR', 0, 'L', $fill);
-            $this->Cell($w[6], 6, $row[6], 'LR', 0, 'L', $fill);
-            $this->Cell($w[7], 6, $row[7], 'LR', 0, 'L', $fill);
-            $this->Cell($w[8], 6, $row[8], 'LR', 0, 'L', $fill);
-            $this->Cell($w[9], 6, $row[9], 'LR', 0, 'L', $fill);
+           
             $this->Ln();
             $fill=!$fill;
         }
@@ -154,7 +142,7 @@ $pdf->SetFont('helvetica', '', 8);
 $pdf->AddPage();
 
 // column titles
-$header = array('User Name', 'Address', 'Mobile', 'Start Date', 'End Date', 'Actual Ltrs', 'Ltr Price', 'Extra Ltrs', 'Cancelled Ltrs', 'Total');
+$header = array('Vendor Name', 'Product Name', 'Total Ltrs');
 
 // data loading
 $data = $pdf->LoadData();
